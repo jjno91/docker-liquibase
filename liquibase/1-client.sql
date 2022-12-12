99076ff259744bccf700747e0f5ef5d6809a2348
Jeremiah Norris <jjno91@gmail.com>
2022-12-12 17:17:01 -0600

    add properties file

--- liquibase/1-client.sql
+++ liquibase/1-client.sql
@@ -1,11 +1,19 @@
 --liquibase formatted sql
---changeset root:1
 
---rollback DROP TABLE client;
-create table client (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
+--changeset root:1
+create table temp1 (
+                        id int primary key not null,
+                        name varchar(50) not null,
+                        address1 varchar(50),
+                        address2 varchar(50),
+                        city varchar(30)
+)
+
+--changeset root:2
+create table temp2 (
+                        id int primary key not null,
+                        name varchar(50) not null,
+                        address1 varchar(50),
+                        address2 varchar(50),
+                        city varchar(30)
 )
