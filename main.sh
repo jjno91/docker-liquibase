998a0dda7c885280f9a519936a3e5b42fdf028f3
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 11:50:00 -0500

    variablize DATABASE_NAME

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ liquibase update \
           --url "jdbc:sqlserver://${DATABASE_ADDRESS};encrypt=false" \
           --username "${DATABASE_USERNAME}" \
           --password "${DATABASE_PASSWORD}" \
+          --database "${DATABASE_NAME:-master}" \
           --changelog-file liquibase/1-client.sql \
-          --database test \
           --log-level FINE
 
 # start your application
