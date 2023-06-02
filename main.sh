35f59ff2b2831ec04823b3210e0f373b0ff8403c
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 09:37:39 -0500

    reformat args

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ liquibase update \
           --url "jdbc:sqlserver://${DATABASE_ADDRESS}" \
           --username "${DATABASE_USERNAME}" \
           --password "${DATABASE_PASSWORD}" \
-          --driver "com.microsoft.sqlserver.jdbc.SQLServerDriver" \
-          --changelog-file=liquibase/1-client.sql \
-          --log-level "debug"
+          --driver com.microsoft.sqlserver.jdbc.SQLServerDriver \
+          --changelog-file liquibase/1-client.sql \
+          --log-level FINE
 
 # start your application
