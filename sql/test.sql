56ce2f4d7c75611cf47cbb84d3e012a173a3ccd0
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 12:42:38 -0500

    add test.sql

---
+++ sql/test.sql
@@ -0,0 +1,22 @@
+--liquibase formatted sql
+
+--changeset test:1
+create table test2 (
+    id int primary key not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset test:2
+create table test3 (
+    id int primary key not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset test:3
+drop table test3
