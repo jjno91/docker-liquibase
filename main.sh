6ea001cfe3d1d06f5f1127188be08a4c67ee0678
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 09:07:21 -0500

    remove the offline flag

--- main.sh
+++ main.sh
@@ -4,7 +4,6 @@ liquibase update \
           --url "jdbc:sqlserver://${DATABASE_ADDRESS}:1433;databaseName=${DATABASE_NAME}" \
           --username "${DATABASE_USERNAME}" \
           --password "${DATABASE_PASSWORD}" \
-          --changelog-file=liquibase/1-client.sql \
-          --offline
+          --changelog-file=liquibase/1-client.sql
 
 # start your application
