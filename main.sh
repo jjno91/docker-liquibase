bc25e2123e958f364d8250b86f4ea43b6d27ad4a
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 11:54:40 -0500

    move database name to url

--- main.sh
+++ main.sh
@@ -2,10 +2,9 @@
 
 # https://www.liquibase.org/get-started/best-practices
 liquibase update \
-          --url "jdbc:sqlserver://${DATABASE_ADDRESS};encrypt=false" \
+          --url "jdbc:sqlserver://${DATABASE_ADDRESS};databaseName=${DATABASE_NAME:-master};encrypt=false" \
           --username "${DATABASE_USERNAME:-sa}" \
           --password "${DATABASE_PASSWORD}" \
-          --database "${DATABASE_NAME:-master}" \
           --changelog-file liquibase/1-client.sql \
           --log-level FINE
 
