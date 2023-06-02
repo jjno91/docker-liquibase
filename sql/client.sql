2b75bc862e88c40afd57910422a92f2c6e52e87d
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 12:38:33 -0500

    rename sql

--- sql/client.sql
+++ sql/client.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset root:2
+--changeset client:1
 create table temp2 (
     id int primary key not null,
     name varchar(50) not null,
@@ -9,7 +9,7 @@ create table temp2 (
     city varchar(30)
 )
 
---changeset root:3
+--changeset client:2
 create table temp3 (
     id int primary key not null,
     name varchar(50) not null,
