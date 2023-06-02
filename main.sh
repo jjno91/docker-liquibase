59084a3cf3f64c43ed4029f7c66eacb61fda90e1
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 12:00:34 -0500

    variablize DATABASE_ENCRYPT

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 # https://www.liquibase.org/get-started/best-practices
 liquibase update \
-          --url "jdbc:sqlserver://${DATABASE_ADDRESS};databaseName=${DATABASE_NAME:-master};encrypt=false" \
+          --url "jdbc:sqlserver://${DATABASE_ADDRESS};databaseName=${DATABASE_NAME:-master};encrypt=${DATABASE_ENCRYPT:-true}" \
           --username "${DATABASE_USERNAME:-sa}" \
           --password "${DATABASE_PASSWORD}" \
           --changelog-file liquibase/1-client.sql \
