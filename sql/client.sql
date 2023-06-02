022f06ca355fcb4ebbbf7df8378e7d039f218826
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 13:10:38 -0500

    duplicate changeset in file

--- sql/client.sql
+++ sql/client.sql
@@ -18,5 +18,14 @@ create table temp3 (
     city varchar(30)
 )
 
+--changeset test:2
+create table temp3 (
+    id int primary key not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
 --changeset test:3
 drop table temp3
