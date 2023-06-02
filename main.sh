79a6a9272d7654ab0c55e58ae8df337d180d50d7
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 08:30:56 -0500

    refactor liquibase command

--- main.sh
+++ main.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-liquibase --changelog-file=liquibase/1-client.sql update --url "${URL}" --username "${USERNAME}" --password "${PASSWORD}"
+liquibase update \
+          --url "jdbc:sqlserver://${DATABASE_ADDRESS}:1433;databaseName=${DATABASE_NAME}" \
+          --username "${DATABASE_USERNAME}" \
+          --password "${DATABASE_PASSWORD}" \
+          --changelog-file=liquibase/1-client.sql \
+          --hub-mode=off
 
 # start your application
