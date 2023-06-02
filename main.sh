56fbb4152751277a7fa9651a1660031f5c5eca26
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 09:31:47 -0500

    remove driver from url

--- main.sh
+++ main.sh
@@ -2,9 +2,10 @@
 
 # https://www.liquibase.org/get-started/best-practices
 liquibase update \
-          --url "jdbc:sqlserver://${DATABASE_ADDRESS}" \
+          --url "${DATABASE_ADDRESS}" \
           --username "${DATABASE_USERNAME}" \
           --password "${DATABASE_PASSWORD}" \
+          --driver "com.microsoft.sqlserver.jdbc.SQLServerDriver" \
           --changelog-file=liquibase/1-client.sql
 
 # start your application
