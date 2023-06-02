1676fec152e7752292be7d199e62cffd05215b1d
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 09:05:45 -0500

    change dialect and fix whitespace

--- liquibase/1-client.sql
+++ liquibase/1-client.sql
@@ -1,20 +1,19 @@
 --liquibase formatted sql
--- dialect: tsql
 
 --changeset root:2
 create table temp2 (
-                       id int primary key not null,
-                       name varchar(50) not null,
-                       address1 varchar(50),
-                       address2 varchar(50),
-                       city varchar(30)
+    id int primary key not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
 )
 
 --changeset root:3
 create table temp3 (
-                       id int primary key not null,
-                       name varchar(50) not null,
-                       address1 varchar(50),
-                       address2 varchar(50),
-                       city varchar(30)
+    id int primary key not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
 )
