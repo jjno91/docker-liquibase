ceaf89b997a3ae37ec5c698c1e06c9c0fa064f0f
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 09:37:10 -0500

    add driver back to URL

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 # https://www.liquibase.org/get-started/best-practices
 liquibase update \
-          --url "${DATABASE_ADDRESS}" \
+          --url "jdbc:sqlserver://${DATABASE_ADDRESS}" \
           --username "${DATABASE_USERNAME}" \
           --password "${DATABASE_PASSWORD}" \
           --driver "com.microsoft.sqlserver.jdbc.SQLServerDriver" \
