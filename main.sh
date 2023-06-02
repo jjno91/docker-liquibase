2be3deba513f3c0dde717a902e11d9481c9a35e8
Jeremiah Norris <jjno91@users.noreply.github.com>
2023-06-02 11:52:55 -0500

    set default DATABASE_USERNAME

--- main.sh
+++ main.sh
@@ -3,10 +3,11 @@
 # https://www.liquibase.org/get-started/best-practices
 liquibase update \
           --url "jdbc:sqlserver://${DATABASE_ADDRESS};encrypt=false" \
-          --username "${DATABASE_USERNAME}" \
+          --username "${DATABASE_USERNAME:-sa}" \
           --password "${DATABASE_PASSWORD}" \
           --database "${DATABASE_NAME:-master}" \
           --changelog-file liquibase/1-client.sql \
           --log-level FINE
 
 # start your application
+echo "start your application"
